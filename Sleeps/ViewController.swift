c7958fe876fa020f6bf9c0199880dec6c92d46e0
Josh Asch <josh.asch@gmail.com>
2015-03-30 17:47:05 +0100

    Initial Commit

---
+++ Sleeps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sleeps
+//
+//  Created by Josh Asch on 30/03/2015.
+//  Copyright (c) 2015 Bearhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
