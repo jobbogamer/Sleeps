b779e8e060e35746d3bee8423ba6f11ea127d442
Josh Asch <josh.asch@gmail.com>
2015-03-30 23:15:20 +0100

    Set up storyboard with split view, custom classes

---
+++ Sleeps/CountdownTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CountdownTableViewController.swift
+//  Sleeps
+//
+//  Created by Josh Asch on 30/03/2015.
+//  Copyright (c) 2015 Bearhat. All rights reserved.
+//
+
+import UIKit
+
+class CountdownTableViewController: UITableViewController {
+
+}
