cc15859b90ff383860e460ea239e8e713e5a3d2c
Josh Asch <josh.asch@gmail.com>
2015-04-05 12:55:13 +0100

    Start better interface

--- Sleeps/CountdownCollectionViewController.swift
+++ Sleeps/CountdownCollectionViewController.swift
@@ -8,6 +8,6 @@
 
 import UIKit
 
-class CountdownTableViewController: UITableViewController {
+class CountdownCollectionViewController: UICollectionViewController {
 
 }
---
+++ Sleeps/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Sleeps
+//
+//  Created by Josh Asch on 05/04/2015.
+//  Copyright (c) 2015 Bearhat. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+}
