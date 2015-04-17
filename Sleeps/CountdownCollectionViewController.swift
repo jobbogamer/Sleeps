cf0c6fda81664e52e9c87001d730526805af2f34
Josh Asch <josh.asch@gmail.com>
2015-04-17 18:40:22 +0100

    Stub out delegate and datasource functions for CCVC

--- Sleeps/CountdownCollectionViewController.swift
+++ Sleeps/CountdownCollectionViewController.swift
@@ -8,6 +8,36 @@
 
 import UIKit
 
-class CountdownCollectionViewController: UICollectionViewController {
-
+class CountdownCollectionViewController: UICollectionViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    // MARK: - UIViewController
+    
+    override func viewDidLoad() {
+        // TODO: Load the latest data from the database.
+    }
+    
+    
+    // MARK: - UICollectionViewDataSource
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // TODO: Return the number of items fetched from the database.
+        return 0
+    }
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // Always return 1, because there are no logical groups or sections in the data.
+        return 1
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        // TODO: Dequeue a cell, fill it with goodness.
+        return UICollectionViewCell()
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // TODO: Perform a segue to the single-item view or editing view.
+    }
+    
 }
---
+++ Sleeps/CountdownNumberView.swift
@@ -0,0 +1,21 @@
+//
+//  CountdownNumberView.swift
+//  Sleeps
+//
+//  Created by Josh Asch on 05/04/2015.
+//  Copyright (c) 2015 Bearhat. All rights reserved.
+//
+
+import UIKit
+
+class CountdownNumberView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
