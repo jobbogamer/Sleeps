66367f799243d1ef413decec01ca7c271561d0cf
Josh Asch <josh.asch@gmail.com>
2015-05-24 23:10:39 +0100

    Give Edit and Share buttons a 1pt outline

--- Sleeps/EditViewController.swift
+++ Sleeps/EditViewController.swift
@@ -12,11 +12,22 @@ class EditViewController: UIViewController {
     
     /// The countdown that's being edited.
     var countdown: Countdown!
+    
+    /// The Edit button in the bottom left.
+    @IBOutlet weak var editButton: RoundedCornerButton!
+    
+    /// The Share button in the bottom right.
+    @IBOutlet weak var shareButton: RoundedCornerButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        // Give the Edit and Share buttons an outline.
+        editButton.layer.borderColor  = kGlobalTintColour.CGColor
+        editButton.layer.borderWidth  = 1
+        shareButton.layer.borderColor = kGlobalTintColour.CGColor
+        shareButton.layer.borderWidth = 1
     }
 
 }
